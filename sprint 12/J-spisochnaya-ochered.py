877fedbf4cfda6783444023823f6f9e13b51ca9e
Анна МительковаÐ <221882@mail.ru>
2023-02-27 16:15:50 +0300

    finished spisochnaya-ochered

--- sprint 12/J-spisochnaya-ochered.py
+++ sprint 12/J-spisochnaya-ochered.py
@@ -7,3 +7,42 @@
 # put(x) — добавить число x в очередь
 # size() — вывести текущий размер очереди
 
+# В первой строке записано количество команд n — целое число,
+# не превосходящее 1000. В каждой из следующих n строк записаны команды по
+# одной строке.
+
+class ListQueue:
+    def __init__(self):
+        self.queue = []
+        self.head = 0
+        self.size = 0
+
+    def get(self):
+        if self.size:
+            print(self.queue[self.head])
+            self.queue.pop(self.head)
+            self.size -= 1
+        else:
+            print('error')
+
+    def put(self, item):
+        self.queue.append(item)
+        self.size += 1
+
+    def get_size(self):
+        return self.size
+
+
+if __name__ == '__main__':
+    num_commands = int(input())  # количество команд    
+
+    q = ListQueue()
+
+    for i in range(num_commands):
+        s = input().split()
+        if s[0] == 'get':
+            q.get()
+        elif s[0] == 'put':
+            q.put(s[1])
+        elif s[0] == 'size':
+            print(q.get_size())
