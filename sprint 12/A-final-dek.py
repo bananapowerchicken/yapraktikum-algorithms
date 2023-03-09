a78b1722d34cdb4901cead12a146a87c59a713f8
Анна МительковаÐ <221882@mail.ru>
2023-03-09 17:53:01 +0300

    fix A-sprint 12 final review1

--- sprint 12/A-final-dek.py
+++ sprint 12/A-final-dek.py
@@ -13,15 +13,14 @@ class Deque:
 
     def push_front(self, item):
         if self.size == self.max_n:
-            raise OverflowError
+            raise ValueError('Deque is full')        
         self.buff[self.head - 1] = item
         self.head = (self.head - 1) % self.max_n
         self.size += 1
 
     def push_back(self, item):
         if self.size == self.max_n:
-            raise OverflowError  #  на что можно заменить?
-
+            raise ValueError('Deque is full')
         self.buff[self.tail] = item
         self.tail = (self.tail + 1) % self.max_n
         self.size += 1
@@ -57,29 +56,15 @@ if __name__ == '__main__':
 
     d = Deque(max_buff_size)
 
-    # commands_dict = {
-    #     'push_front': d.push_front,
-    #     'push_back': d.push_back,
-    #     'pop_front': d.pop_front,
-    #     'pop_back': d.pop_back,
-    # }
-
     for _ in range(num_commands):
         command = input()
         operation, *val = command.split()
-        if val:
-            try:
-                res = getattr(d, operation)(*val)
-                # res = commands_dict[operation](int(*val))
-                if res is not None:
-                    print(res)
-            except OverflowError:
-                print('error')
-        else:
-            try:
-                res = getattr(d, operation)()
-                # res = commands_dict[operation]()
-                if res is not None:
-                    print(res)
-            except IndexError:
-                print('error')
+
+        try:
+            res = getattr(d, operation)(*val)
+            if res is not None:
+                print(res)
+        except OverflowError:
+            print('error')
+        except IndexError:
+            print('error')
